16073900313aa2d06ca86f8bdbc085321e12ead8
Moreno <mauro.lider.123@gmail.com>
2025-09-07 19:53:32 -0500

    Crear controladores para gestionar vehículos y contactos

---
+++ app/Http/Controllers/ContactoController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Contacto;
+
+class ContactoController extends Controller
+{
+    public function index()
+    {
+        $contactos = Contacto::all();
+        return view('contactos.index', compact('contactos'));
+    }
+
+    public function create()
+    {
+        return view('contactos.create-contacto');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate($this->validationRules(), $this->validationMessages());
+
+        Contacto::create($request->all());
+        return redirect()->route('contactos.index')->with('success', 'Cliente registrado con éxito!');
+    }
+
+    public function edit($id)
+    {
+        $contacto = Contacto::findOrFail($id);
+        return view('contactos.create-contacto', compact('contacto'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate($this->validationRules($id), $this->validationMessages());
+
+        $contacto = Contacto::findOrFail($id);
+        $contacto->update($request->all());
+        return redirect()->route('contactos.index')->with('success', 'Cliente actualizado con éxito!');
+    }
+
+    public function destroy($id)
+    {
+        Contacto::destroy($id);
+        return redirect()->route('contactos.index')->with('success', 'Cliente eliminado con éxito!');
+    }
+
+    private function validationRules($id = null)
+    {
+        return [
+            'nombre' => 'required|string|max:255',
+            'apellido' => 'required|string|max:255',
+            'documento_identidad' => 'required|string|max:8|unique:contactos,documento_identidad,' . $id,
+            'correo' => 'required|email',
+            'telefono' => 'required|string|max:9',
+        ];
+    }
+
+    private function validationMessages()
+    {
+        return [
+            'documento_identidad.unique' => 'El DNI ya está registrado en otro contacto.',
+        ];
+    }
+}
